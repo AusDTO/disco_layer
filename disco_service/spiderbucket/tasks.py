9fa9dcfa64fe909f2f17a3e83342c291dcc951d6
Chris Gough <christopher.d.gough@gmail.com>
2015-06-21 00:46:59 +1000

    total massive refactor

--- disco_service/spiderbucket/tasks.py
+++ disco_service/spiderbucket/tasks.py
@@ -1,118 +1,184 @@
-from __future__ import absolute_import
-import pyorient
-import json
-import base64
+"""
+TODO:
+    we do need to go the other way as well,
+    delete from SOLR and the DB when page is "absent" from OrientDB
+    and by absent, I mean flagged as missing (not available)
+    or whatever (ask Nigel about business logic)
+
+#def purge_url_rdbms(url):
+"""
 from celery import shared_task
+from django.test import TestCase
 from .models import Page
+from celery.utils.log import get_task_logger
 
-class ODBWrapper:
-    # this should be coming from settings!
-    DBHOST='52.64.24.77'
-    DBPORT=2424
-    DBUSR='root'
-    DBPSX='developmentpassword'
-    DBNAME = 'webContent'
-    client = pyorient.OrientDB(DBHOST, DBPORT)
-    client.db_open(DBNAME, DBUSR, DBPSX)
-    
-    def __init__(self):
-        self.client = ODBWrapper.client
-    
-    def all_webDocumentContainers_callback(self, count, callback):
-        self.client.query_async("select from webDocumentContainer", count, '*:0', callback)    
-    
-    def count_webdocumentContainers(self, url):
-        query = "select count(*) from webDocumentContainer where url=\"http://google.com/\""# % url
-        result = self.client.query(query, 1, '*:0')
-        return result[0].oRecordData['count']
+class MultiplePagesWithSameURLError(Exception): pass
+class InvalidPageDataError(Exception): pass
+class InvalidPageIDError(Exception): pass
 
-    def count_all_records(self):
-        return self.client.db_count_records()
 
-class RDBMSWrapper:
-    def __init__(self):
-        self.foo = 'BAR'
-    def page_with_url_exists(self, page_url):
-        num_found = Page.objects.filter(url=page_url).count()
-        if num_found > 0:
-            return True
-        else:
+class PageDictValidator(object):
+    '''
+    This captures/encapsulates our assumptions about the
+    webDocumentContainer data in OrientDB
+    '''
+    REQUIRED_PROPERTIES = (
+        'orient_rid', 'orient_version',
+        'orient_class', 'url')
+    ALLOWED_PROPERTIES = (
+        'protocol', 'host', 'port',
+        'depth', 'fetched',
+        'status', 'lastFetchDateTime',
+        'nextFetchDateTime', 'document')
+    def valid(self, page):
+        if type(page) != type ({}):
             return False
-    
+        for k in self.REQUIRED_PROPERTIES:
+            if k not in page.keys():
+                return False
+        return True
+
+
 @shared_task
-def sync_url_from_orient(url):
-    ''' Idempotently sync one page in RDBMS/Solr from OrientDB
-    
-        e.g. following document insert/update in OrientDB
+def push_page_dict(page):
     '''
-    ow = ODBWrapper()
-    if ow.count_webdocumentContainers(url) == 0:
-        # corner-case: page absent from OrientDB
-        purge_url_rdbms.delay(url)
-        purge_url_solr.delay(url)
+    Idempotently sync one page in RDBMS/Solr,
+    
+    presumably sourced from OrientDB following insert/update
+    but we have rendered the data to a dict
+    so there is no trace of pyorient anymore.
+    '''
+    pvd = PageDictValidator()
+    if not pvd.valid(page):
+        if 'document' in page.keys():
+            if len(page['document']) > 100:
+                page['document'] = page['document'][:100] + "... 8< chomped."
+        raise InvalidPageDataError, page
+    num_found = Page.objects.filter(url=page['url']).count()
+    if num_found == 1:
+        update_page_in_db.delay(page)
+    elif num_found == 0:
+        insert_page_in_db.delay(page)
     else:
-        rdb = RDBMSWrapper()
-        if rdb.page_with_url_exists(url):
-            rdbms_id = rdb.update_page('foo')
-        else:
-            rdbms_id = rdb.insert_page('foo')
-        sync_solr_from_rdbms.delay(rdbms_id)
-
+        raise MultiplePagesWithSameURLError, page['url']
 
 @shared_task
-def purge_url_rdbms(url):
-    ''' Delete document from RDBMS after deletion from OrientDB '''
-    ## GUARD CONDITION
-    # if a document exists in OrientDB with that url:
-    #   fail! dispatch sync_url_from_orient(url)
-    ## LAZY is good
-    # else if the document does not exist in the RDBMS:
-    #   do nothing
-    # else:
-    #   delete it
-    #   purge_solr_url(url)
-    print "TODO: purge_url_rdbms(%s)" % url
-    pass
+def update_page_in_db(page):
+    pvd = PageDictValidator()
+    if not pvd.valid(page):
+        raise InvalidPageDataError, page
+    p = Page.objects.get(url=page['url'])  # url is imutable
+    dirty = False
+    for prop in pvd.REQUIRED_PROPERTIES + pvd.ALLOWED_PROPERTIES:
+        exec("m = p.%s" % prop)
+        if prop in page.keys():
+            if m != page[prop]:
+                m = page[prop]
+                dirty = True
+        elif m is not None:
+            m = None
+            dirty = True
+    if dirty:
+        p.save()
+    sync_page_sinks.delay(p.id)
 
 @shared_task
-def sync_solr_from_rdbms(rdbms_document_id):
-    ''' Ensure solr is up to date with the RDBMS, for the given document'''
-    print "TODO: sync_solr_from_rdbms(%s)" % rdbms_document_id
+def insert_page_in_db(page):
+    pvd = PageDictValidator()
+    if not pvd.valid(page):
+        raise InvalidPageDataError, page
+    p = Page( url=page['url'],
+              orient_rid=page['orient_rid'],
+              orient_version=page['orient_version'],
+              orient_class=page['orient_class'])
+    for prop in pvd.ALLOWED_PROPERTIES:
+        exec("m = p.%s" % prop)
+        if prop in page.keys():
+            m = page[prop]
+    p.save()
+    sync_page_sinks.delay(p.id)
 
-@shared_task
-def purge_url_solr(url):
-    ''' remove the document with given url from solr '''
-    print "TODO: purge_url_solr(%s)" % url
 
-
-@shared_task
-def sync_the_db(limit=None):
-    ''' A periodic task that syncs the RDBMS from OrientDB
-
-    Obsolete when we have OriendDB making a callback to a web method.
-
-    Intended to be called on a schedule (e.g. by celerybeat)
+class NulTask:
     '''
-    ### do this bit now
-    # for each document in OrientDB that should be in the database:
-    #    dispatch a job to ensure it is valid in the DB
+    like a @shared_task except won't burden the queue with a useless message 
+    '''
+    def init(self, page=None):
+        pass
+    @classmethod
+    def delay(self, page):
+        pass
 
-    ### do this bit later
-    # for each service in the catalogue (?)
-    #    dispatch a job to ensure it is valid in the DB
-    def _process_webdocument(wdc):
-        ''' this is stupid. I'm given an object,
-            I pick it's ID, and dispatch that to an async
-            task that uses the ID to retrieve the same object again.
 
-            would be smarter to pass this guy an ID only
-        '''
-        if 'url' in wdc.oRecordData.keys():
-            if 'document' in wdc.oRecordData.keys():
-                url = wdc.oRecordData['url']
-                doc_sync_orient_cru.delay(url)
-    ow = ODBWrapper()
-    if not limit:
-        limit = 2+ow.count_all_records()
-    ow.all_webDocumentContainers_callback(limit, _process_webdocument)
+class PageSyncSubscribers(object):
+    '''
+    This is a bit of a white elephant.
 
+    I was synchronising Solr from here until I discovered this:
+    https://github.com/django-haystack/celery-haystack/
+
+    Now I'm just keeping this here in case I want to push/purge
+    each delta to other sinks (e.g. tripplestore, event log,
+    semantic agents). Or if I get cranky with celery-haystack.
+    '''
+    # this might be better in settings.py
+    HARDCODED_CONFIG = (
+        {
+            'name':'solr',
+            'push': 'NulTask',
+            'delete': 'NulTask'
+        },
+    )
+
+    def list_push_task_names(self):
+        out = []
+        for c in self.HARDCODED_CONFIG:
+            out.append(c['push'])
+        return out
+
+    def list_push_tasks(self):
+        out = []
+        for taskname in self.list_push_task_names():
+            eval("out.append(%s)" % taskname)
+        return out
+
+    def list_delete_task_names(self):
+        out = []
+        for c in self.HARDCODED_CONFIG:
+            out.append(c['delete'])
+        return out
+
+    def list_delete_tasks(self):
+        out = []
+        for taskname in self.list_delete_task_names():
+            eval("out.append(%s)" % taskname)
+        return out
+
+    def list_subscriber_names(self):
+        out = []
+        for c in self.HARDCODED_CONFIG:
+            out.append(c['name'])
+        return out
+
+@shared_task
+def sync_page_sinks(page_id):
+    '''
+    after updating a pages, dispatching a call to sync_page_sinks.
+    
+    This task provides indirection between database wrtites and consequent
+    updates to downstream data sources. i.e. extensability pattern.
+
+    PageSyncSubscribers is responsible for knowing who to dispatch to.
+
+    As with harvest_celery, it might be better to drive this from signals.
+    '''
+    if type(page_id) != type(1):
+        raise InvalidPageIDError, page_id
+    pss = PageSyncSubscribers()
+    if Page.objects.filter(pk=page_id).count() == 0:
+        for t in pss.list_delete_tasks():
+            t.delay(page_id)
+    else:
+        for t in pss.list_push_tasks():
+            page = Page.objects.get(pk=page_id)
+            t.delay(page)
