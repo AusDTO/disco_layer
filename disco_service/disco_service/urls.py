67d9566756b3fb65ea4a57d01191c2d3211f5247
Chris Gough <christopher.d.gough@gmail.com>
2015-06-26 00:34:48 +1000

    fix greedy urls

--- disco_service/disco_service/urls.py
+++ disco_service/disco_service/urls.py
@@ -4,9 +4,9 @@ import spiderbucket.views
 
 urlpatterns = [
     url(r'^$', spiderbucket.views.index, name='home'),
-    url(r'^search/', spiderbucket.views.search, name='search'),
-    url(r'^api/', spiderbucket.views.api, name='api'),
-    url(r'^for_gov/', spiderbucket.views.for_gov, name='for_gov'),
+    url(r'^search/$', spiderbucket.views.search, name='search'),
+    url(r'^api/$', spiderbucket.views.api, name='api'),
+    url(r'^for_gov/$', spiderbucket.views.for_gov, name='for_gov'),
     url(r'^intro/', include('spiderbucket.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^s/', include('haystack.urls')),
