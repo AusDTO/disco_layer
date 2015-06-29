229c8eb9027fa23ff205149fd6494449d577f44f
Chris Gough <christopher.d.gough@gmail.com>
2015-06-29 17:30:57 +1000

    DB migration to create agency

---
+++ disco_service/govservices/migrations/0011_agency.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('govservices', '0010_auto_20150628_1334'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Agency',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('acronym', models.CharField(max_length=128)),
+            ],
+        ),
+    ]
