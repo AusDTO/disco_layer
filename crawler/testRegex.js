aaa8b3623c032670f9a600023fb219841b7f8189
nokout <nigel.okeefe@gmail.com>
2015-07-01 19:29:03 +1000

    Fixed Commit

---
+++ crawler/testRegex.js
@@ -0,0 +1,21 @@
+var hosts = [
+    "humanservices.gov.au",
+    "environment.gov.au",
+    "ramint.gov.au",
+    "nfsa.gov.au",
+    "sa.gov.au",
+    "vic.gov.au",
+    "www.vic.gov.au",
+    "something.qld.gov.au",
+    "www.something.wa.gov.au",
+    "something.wa.gov.au",
+    "nt.gov.au",
+]
+var stateSubDomain = /(^|\.)vic\.gov\.au$|(^|\.)nsw\.gov\.au$|(^|\.)qld\.gov\.au$|(^|\.)tas\.gov\.au$|(^|\.)act\.gov\.au$|(^|\.)sa\.gov\.au$|(^|\.)wa\.gov\.au$|(^|\.)nt\.gov\.au$/;
+//var stateDomain = /^vic\.gov\.au$|^nsw\.gov\.au$|^qld\.gov\.au$|^tas\.gov\.au$|^act\.gov\.au$|^sa\.gov\.au$|^wa\.gov\.au$|^nt\.gov\.au$/;
+
+hosts.forEach(function(host) {
+
+    console.log("Host: " + host + "\t SubDomain Outcome: " + (host.search(stateSubDomain) < 0));
+    //      	console.log("Host: " + host + "\t Domain Outcome: " + (host.search(stateDomain) <0 ) + "\n");
+});
