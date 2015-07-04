f0c4be7e3c0430f3bee557f7fced54f5996902da
Chris Gough <christopher.d.gough@gmail.com>
2015-07-05 00:22:11 +1000

    increase agression of test_on_save, -d --failfast

--- disco_service/govservices/test_on_save.sh
+++ disco_service/govservices/test_on_save.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while inotifywait -e close_write govservices/* govservices/management/* govservices/management/commands/* test_fixtures/*
 do
-    python manage.py test govservices
+    python manage.py test govservices --failfast -d
 done
