c3ca6478685425b474886b5a844113d52bfee8f2
nokout <nigel.okeefe@gmail.com>
2015-07-11 16:17:35 +1000

    Added filter to exclude trove until further resolution is determined. Renamed old db files. Set fliped job to log seperately

--- crawler/flipWrapper.sh
+++ crawler/flipWrapper.sh
@@ -2,4 +2,4 @@
 #This is called by the cron job. Sets up environment and runs job
 export $(cat /src/crawler.env | xargs)
 env | grep CRAWL_
-cd /src; /usr/local/bin/node /src/crawl.js --fliporder
+cd /src; /usr/local/bin/node /src/crawl.js --fliporder -- logfile /src/logs/flipcrawl
