90c2f144f123bdb781909f85a8003878c953ce92
Chris Gough <christopher.d.gough@gmail.com>
2015-07-14 10:35:02 +1000

    refactor disco service into a single Dockerfile. Now, environment variable DISCO_ROLE=WORKER makes the container into a headless worker node, otherwise it's a web front end node

---
+++ disco_service/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# if DISCO_ROLE==WORKER, then run as a celery node
+# otherwise run as a front-end node
+#
+LOGLEVEL=info
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+mkdir ./logs
+touch ./logs/access.log
+touch ./logs/gunicorn.log
+tail -n 0 -f ./logs/*.log &
+
+if [ "$(DISCO_ROLE)" -eq "WORKER" ]
+then:
+    exec python manage.py celery worker -l info "$@"
+else:
+    exec gunicorn disco_service.wsgi:application \
+	--name disco_service \
+	--bind 0.0.0.0:8000 \
+	--workers 3 \
+	--log-level=$LOGLEVEL \
+	--log-file=./logs/gunicorn.log \
+	--access-logfile=./logs/access.log \
+	"$@"
+fi
