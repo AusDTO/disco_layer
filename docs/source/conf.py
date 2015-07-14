08925b8c5002236b1bf611f72eb26a59fa0dbedc
Chris Gough <christopher.d.gough@gmail.com>
2015-07-14 11:10:42 +1000

    stub documents

---
+++ docs/source/conf.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+import sys
+import os
+import shlex
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.graphviz',
+]
+
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'Discovery Layer'
+copyright = u'2015, AusDTO'
+author = u'AusDTO'
+version = '0.1'
+release = '0.1.0'
+language = None
+exclude_patterns = []
+pygments_style = 'sphinx'
+todo_include_todos = False
+html_theme = 'alabaster'
+html_static_path = ['_static']
+htmlhelp_basename = 'DiscoveryLayerdoc'
+
+latex_elements = {
+'papersize': 'a4paper',
+}
+
+latex_documents = [
+  (master_doc, 'DiscoveryLayer.tex', u'Discovery Layer Documentation',
+   u'AusDTO', 'manual'),
+]
+
+texinfo_documents = [
+  (master_doc, 'DiscoveryLayer', u'Discovery Layer Documentation',
+   author, 'DiscoveryLayer', 'One line description of project.',
+   'Miscellaneous'),
+]
---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+.. Discovery Layer documentation master file, created by
+   sphinx-quickstart on Tue Jul 14 10:29:06 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Discovery Layer's documentation!
+===========================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
