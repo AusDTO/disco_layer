5906c846ca666b2dd184733d915e57da34c98959
Chris Gough <christopher.d.gough@gmail.com>
2015-07-24 20:28:26 +1000

    disco_service: stop serving the obsolete URLs

--- disco_service/disco_service/urls.py
+++ disco_service/disco_service/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^search/$', haystack.views.SearchView(), name='search'),
     url(r'^api/$', spiderbucket.views.api, name='api'),
     url(r'^for_gov/$', spiderbucket.views.for_gov, name='for_gov'),
-    url(r'^intro/', include('spiderbucket.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^intro/', include('spiderbucket.urls')),
+    #url(r'^admin/', include(admin.site.urls)),
     #url(r'^search/', include('haystack.urls')),
 ]
