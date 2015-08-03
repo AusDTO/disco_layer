b337f42727df583001a60d0715290580df714498
Chris Gough <christopher.d.gough@gmail.com>
2015-08-03 23:55:36 +1000

    move sync_from_crawler to crawler.tasks (fixes 62)

---
+++ disco_service/crawler/tasks.py
@@ -0,0 +1,25 @@
+from celery import shared_task
+from django.db import connection
+from .models import WebDocument
+from metadata.tasks import insert_resource_from_row
+
+@shared_task
+def sync_from_crawler(limit=1000):
+    raw_sql = '''
+        select
+            url, _hash, protocol, contenttype,
+            host, port, path, depth, "lastFetchDateTime"
+        from "webDocuments"
+        where "fetchStatus" = 'downloaded'
+        and url not in (
+            select url
+            from metadata_resource
+        )'''
+    if limit is not None:
+        raw_sql += ' limit = %s' % int(limit)
+    cursor = connection.cursor()
+    cursor.execute(raw_sql)
+    for row in cursor:
+        insert_resource_from_row.delay(row)
+
+
