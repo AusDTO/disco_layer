c13b9b3a614419c8b3f1a8502460b86433e198e6
Chris Gough <christopher.d.gough@gmail.com>
2015-08-05 18:32:54 +1000

    disco_service package documentation

--- disco_service/__init__.py
+++ disco_service/__init__.py
@@ -1,5 +1,18 @@
+"""
+Package: disco_service
+======================
+
+This is a django project, containing the usual settings.py, urls.py and wsgi.py 
+
+.. note::
+
+   Also contains `celery.py`, which is configuration for async worker nodes
+
+"""
+
 from __future__ import absolute_import
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_ap
+
