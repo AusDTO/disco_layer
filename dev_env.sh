06a11463e2f73a1b2fdb8c6c6992aefae4409d77
Chris Gough <christopher.d.gough@gmail.com>
2015-08-07 22:26:02 +1000

    dev_env.sh: activate virtualenv too

--- dev_env.sh
+++ dev_env.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 for x in `cat ../ops/alpha_disco/disco_service.env`; do export $x; done
+# assumes you have done `virtualenv .venv`
+# then `pip install -r requirements`
+. .venv/bin/activate
